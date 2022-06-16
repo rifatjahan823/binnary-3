0bb30729f85051d94abc11f63ab16a7ccfcbcd26
rifatjahan823 <rifatjahan823@gmail.com>
2022-06-17 01:57:17 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Froms from './Pages/Froms/Froms';
+
+function App() {
+  return (
+    <div className="App">
+        <Froms></Froms>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Froms/Froms.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const Froms = () => {
+    return (
+        <div >
+            {/* ------input-1----------- */}
+            <div style={{display:"flex",margin:"10px",alignItems:"center",}}>
+                <input style={{border:"0",backgroundColor:"white",padding:"10px 8px",borderRadius:"3px 0px 0px 3px",outline:"0"}} type="email" name="" id="" placeholder='name@gmail.com'/>
+                <div style={{backgroundColor:'white',padding:"7px 8px 7px 0px",borderRadius:"0px 3px 3px 0px",color:'#8B8079'}}>-</div>
+            </div>
+            {/* ------input-2----------- */}
+            <div style={{display:"flex",alignItems:"center",marginLeft:"10px",marginBottom:"5px"}}>
+                <input style={{border:"0",backgroundColor:"white",padding:"10px 8px",borderRadius:"3px 0pc 0px 3px",outline:"0"}} type="email" name="" id="" placeholder='elia@gmail.com'/>
+                <div style={{backgroundColor:'white',padding:"7px 8px 7px 0px",borderRadius:"0px 3px 3px 0px",color:'#8B8079'}}>x</div>
+            </div>
+            <div style={{display:"flex",alignItems:"center",marginLeft:"10px",marginBottom:"5px"}}>
+                <input style={{border:"0",backgroundColor:"white",padding:"10px 8px",borderRadius:"3px 0pc 0px 3px",outline:"0"}} type="email" name="" id="" placeholder='*****'/>
+                <div style={{backgroundColor:'white',padding:"7px 8px 7px 0px",borderRadius:"0px 3px 3px 0px",color:'#8B8079'}}>-</div>
+            </div>
+            {/* ------input-3----------- */}
+            <div style={{display:"flex",alignItems:"center",marginLeft:"10px",marginBottom:"5px"}}>
+             <input style={{border:"0",backgroundColor:"white",padding:"10px 8px",borderRadius:"3px 0pc 0px 3px",outline:"0"}} type="email" name="" id="" placeholder='elia@gmail.com'/>
+            <div style={{backgroundColor:'white',padding:"7px 8px 7px 0px",borderRadius:"0px 3px 3px 0px",color:'#8B8079'}}>x</div>
+            </div>
+            <div style={{display:"flex",alignItems:"center",marginLeft:"10px",marginBottom:"5px"}}>
+                <input style={{border:"0",backgroundColor:"white",padding:"10px 8px",borderRadius:"3px 0pc 0px 3px",outline:"0"}} type="email" name="" id="" placeholder='*****'/>
+                <div style={{backgroundColor:'white',padding:"7px 8px 7px 0px",borderRadius:"0px 3px 3px 0px",color:'#8B8079'}}>x</div>
+            </div>
+            {/* // ----------input-4--------- */}
+            <div style={{display:"flex",alignItems:"center",marginLeft:"10px",marginBottom:"5px"}}>
+                <input style={{border:"0",backgroundColor:"white",padding:"10px 8px",borderRadius:"3px 0pc 0px 3px",outline:"0"}} type="email" name="" id="" placeholder='elia@gmail.com'/>
+                <div style={{backgroundColor:'white',padding:"7px 8px 7px 0px",borderRadius:"0px 3px 3px 0px",color:'#8B8079'}}>xx</div>
+            </div>
+            {/* ------input-5----------- */}
+            <div style={{display:"flex",alignItems:"center",marginLeft:"10px",marginBottom:"5px"}}>
+             <input style={{border:"0",backgroundColor:"white",padding:"10px 8px",borderRadius:"3px 0pc 0px 3px",outline:"0"}} type="email" name="" id="" placeholder='elia@gmail.com'/>
+            <div style={{backgroundColor:'white',padding:"7px 8px 7px 0px",borderRadius:"0px 3px 3px 0px",color:'#8B8079'}}>x</div>
+            </div>
+            <div style={{display:"flex",alignItems:"center",marginLeft:"10px",marginBottom:"5px"}}>
+                <input style={{border:"0",backgroundColor:"white",padding:"10px 8px",borderRadius:"3px 0pc 0px 3px",outline:"0"}} type="email" name="" id="" placeholder='*****'/>
+                <div style={{backgroundColor:'white',padding:"7px 8px 7px 0px",borderRadius:"0px 3px 3px 0px",color:'#8B8079'}}>xx</div>
+            </div>
+        </div>
+        
+    );
+};
+
+export default Froms;
